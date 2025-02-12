143e6d5140663637823d908f67c3856f534c976c
maki <maki.644.io@gmail.com>
2025-02-13 00:26:29 +0900

    Update sed command to work across mac os and linux

--- personalize.sh
+++ personalize.sh
@@ -9,13 +9,22 @@ username=$1
 repository_name=$2
 image_name=$3
 
-find . -type f -name '*.yaml' -exec sed -E -i '' s#https://github.com/[-_a-zA-Z0-9]+#https://github.com/${username}#g {} +
-find . -type f -name '*.yaml' -exec sed -E -i '' s#ghcr.io/[-_a-zA-Z0-9]+#ghcr.io/${username}#g {} +
+# Set sed inline option based on OS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # for macOS
+  SED_IN_PLACE_OPT="-i ''"
+else
+  # for Linux
+  SED_IN_PLACE_OPT="-i"
+fi
+
+find . -type f -name '*.yaml' -exec sed $SED_IN_PLACE_OPT -E "s#https://github.com/[-_a-zA-Z0-9]+#https://github.com/${username}#g" {} +
+find . -type f -name '*.yaml' -exec sed $SED_IN_PLACE_OPT -E "s#ghcr.io/[-_a-zA-Z0-9]+#ghcr.io/${username}#g" {} +
 
 if [ ! -z "$repository_name" ]; then
-  find . -type f -name '*.yaml' -exec sed -E -i '' "s#https://github.com/${username}/[-_a-zA-Z0-9]+#https://github.com/${username}/${repository_name}#g" {} +
+  find . -type f -name '*.yaml' -exec sed $SED_IN_PLACE_OPT -E "s#https://github.com/${username}/[-_a-zA-Z0-9]+#https://github.com/${username}/${repository_name}#g" {} +
 fi
 
 if [ ! -z "$image_name" ]; then
-  find . -type f -name '*.yaml' -exec sed -E -i '' "s#ghcr.io/${username}/[-_a-zA-Z0-9]+#ghcr.io/${username}/${image_name}#g" {} +
+  find . -type f -name '*.yaml' -exec sed $SED_IN_PLACE_OPT -E "s#ghcr.io/${username}/[-_a-zA-Z0-9]+#ghcr.io/${username}/${image_name}#g" {} +
 fi
